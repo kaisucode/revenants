a02093b6210977b39e200a250ea9e5baec92a798
Kaisu <kevin.hsukaihao@gmail.com>
2019-10-12 09:45:27 -0700

    Initial stuff for revive chimera done

--- src/Chimera.js
+++ src/Chimera.js
@@ -1,5 +1,10 @@
 class Chimera extends Entity {
 	constructor(incarnation_x, incarnation_y){
 		super(incarnation_x, incarnation_y);
+		this.whichEntity = "chimera";
+	}
+	render() {
+		fill(0,255,242);
+		ellipse(this.pos.x, this.pos.y, this.dims.x, this.dims.y);
 	}
 }
--- src/sketch.js
+++ src/sketch.js
@@ -5,7 +5,7 @@ let seraphs = [];
 let chimeras = [];
 let entities = [seraphs, chimeras];
 let defenseRadius = 40;
-let seraphSpawnRate = 100;
+let seraphSpawnRate = 1.05;
 
 function setup(){
 	createCanvas(window.innerWidth*0.95, window.innerHeight*0.95);
@@ -13,7 +13,7 @@ function setup(){
 }
 
 function draw(){
-	if (Math.random() < 1/seraphSpawnRate){
+	if (Math.random() > 1/seraphSpawnRate){
 		seraphs.push(new Seraph(Math.random()*width,Math.random()*height));
 	}
 	translate(width/2, height/2);
@@ -26,14 +26,24 @@ function draw(){
 	}
 }
 
-function mouseReleased() {
-	reviveChimera(mouseX-width/2, mouseY-height/2);
+
+function mouseReleased(){
+	if (keyIsDown(83)){		// s key
+		console.log("spawn");
+		reviveChimera(mouseX-width/2, mouseY-height/2);
+	}
+	else
+		selectChimera(mouseX-width/2, mouseY-height/2);
 }
 
-function reviveChimera(posX, posY) {
+function reviveChimera(posX, posY){
 	if (user.souls > 0) {
 		chimeras.push(new Chimera(posX, posY));
 		user.souls--;
 	}
+}
+
+function selectChimera(){
+	console.log("selected chimera");
 }
 
